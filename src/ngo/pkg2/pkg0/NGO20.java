8ddafa5b1f2fffa96b99186f7f647a1b3de5677f
Sabrin631 <sabiriin.aden@icloud.com>
2025-01-07 14:58:03 +0100

    uppdaterade main

--- src/ngo/pkg2/pkg0/NGO20.java
+++ src/ngo/pkg2/pkg0/NGO20.java
@@ -20,7 +20,7 @@ public class NGO20 {
     public static void main(String[] args) {
         
         try{
-            idb = new InfDB("ngo_2024","3306","root","Sommarlov123");
+            idb = new InfDB("ngo_2024","3306","dbAdmin2024","dbAdmin2024PW");
             new Inloggning(idb).setVisible(true);
             
         }catch(InfException ex){
